efb5838551931b9bd68e50c091449b87a87e8f6a
Nico Henglmüller <wi13b091@technikum-wien.at>
2014-06-02 23:38:10 +0200

    Kleine Anpassungen (LoginController CheckLogin gelöscht), Kunde anlegen Workflow hook erstellt. Neues Icon für Kundeanlegen hinzugefügt und eingebaut. Artikel Icon hinzugefügt. Viele neue Icons hinzugefügt und das Kundenbeziehungsmanagement ausgebaut (Use Cases Struktur eingefügt). Bisher keine Geschäftslogik. Versucht das ganze auf zu hübschen, sieht nicht gut aus mit dem sch... css. Merge conflicts.
    Nico

--- index.php
+++ index.php
@@ -45,73 +45,71 @@ foreach (glob(INCS . "*.inc.php") as $filename) {
 
 
 <!DOCTYPE html>
-    <html>
-        <head>
-            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-            <title>Felix Martin Hi-Fi und Videostudios</title>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Felix Martin Hi-Fi und Videostudios</title>
 
-            <link rel="stylesheet" type="text/css" href="public/css/normalize.css"/>
-            <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
-            <link rel="stylesheet" type="text/css" href="public/css/main.css"/>
-            <link rel="stylesheet" type="text/css" href="public/css/form.css">
+    <link rel="stylesheet" type="text/css" href="public/css/normalize.css"/>
+    <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css"/>
+    <link rel="stylesheet" type="text/css" href="public/css/main.css"/>
+    <link rel="stylesheet" type="text/css" href="public/css/form.css">
 
-            <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-            <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
-            <script type="text/javascript" src="public/js/javascript.js"></script>
-        </head>
-    <body>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="public/js/javascript.js"></script>
+</head>
+<body>
 
-        <div id="site"> <!-- GANZE SEITE INKLUSIVE WRAPPER UND FOOTER -->
-            <div id='wrapper'> <!-- HEADER + CONTENT -->
+<div id="site"> <!-- GANZE SEITE INKLUSIVE WRAPPER UND FOOTER -->
+    <div id='wrapper'> <!-- HEADER + CONTENT -->
 
-                <!-- HEADER -->
-                <header id="header">
-                    <!-- BREADCRUMB -->
-                    <div id="breadcrumb" class="clear">
-                        <h1>
-                            <a href="<?php echo URL; ?>">CCR</a>
-                            <?php
-                            if (Session::get("site")) {
-                                for ($i = 0; $i < count($_SESSION['site']); $i++) {
-                                    echo ' | <a href="index.php?section=' . $_SESSION['site'][$i] . '">' . $_SESSION['site'][$i] . '</a>';
-                                }
-                            }
-                            ?>
-                        </h1>
-                    </div>
-
-                    <!-- USER INFO, FALLS EINGELOGGT -->
-                    <?php if(Session::get("username")) : ?>
-                        <div id="userinfo">
-                            als: <?php echo Session::get("vorname") . " " . Session::get("nachname") . ", Abteilung: " . Session::get("rolle"); ?></p>                        </div>
-                    <?php endif; ?>
-
-                    <p><a href="index.php?logout">Logout</a></p>
-            </div>
-            <?php endif; ?>
-            </header>
-
-                <!-- CONTENT -->
-                <div id="content">
+        <!-- HEADER -->
+        <header id="header">
+            <!-- BREADCRUMB -->
+            <div id="breadcrumb" class="clear">
+                <h1>
+                    <a href="<?php echo URL; ?>">CCR</a>
                     <?php
-
-                    if(isset($_GET['section'])) {
-                        if(!Session::get("username")) {
-                            require BOUNDARIES . "login/login.php";
+                    if (Session::get("site")) {
+                        for ($i = 0; $i < count($_SESSION['site']); $i++) {
+                            echo ' | <a href="index.php?section=' . $_SESSION['site'][$i] . '">' . $_SESSION['site'][$i] . '</a>';
                         }
-                        else {
-                            //Hier werden die verschiedenen sections eingetragen == verschiedene HTML Seiten
-                            switch($_GET['section']) {
-                                case "dashboard":
-                                    require BOUNDARIES . "dashboard/dashboard.php";
-                                    break;
+                    }
+                    ?>
+                </h1>
+            </div>
 
-                                /* INCLUDES VON AUFTRAGSMANAGEMENT */
-                                case "artikel":
-                                    require BOUNDARIES . "article/article.php";
-                                    break;
+            <!-- USER INFO, FALLS EINGELOGGT -->
+            <?php if (Session::get("username")) : ?>
+                <div id="userinfo">
+                    als: <?php echo Session::get("vorname") . " " . Session::get("nachname") . ", Abteilung: " . Session::get("rolle"); ?></p>                        </div>
+            <?php endif; ?>
 
-                                /* INCLUDES VON KUNDENBEZIEHUNGSMANAGEMENT */
+            <p><a href="index.php?logout">Logout</a></p>
+
+        </header>
+
+        <!-- CONTENT -->
+        <div id="content">
+            <?php
+
+            if (isset($_GET['section'])) {
+                if (!Session::get("username")) {
+                    require BOUNDARIES . "login/login.php";
+                } else {
+                    //Hier werden die verschiedenen sections eingetragen == verschiedene HTML Seiten
+                    switch ($_GET['section']) {
+                        case "dashboard":
+                            require BOUNDARIES . "dashboard/dashboard.php";
+                            break;
+
+                        /* INCLUDES VON AUFTRAGSMANAGEMENT */
+                        case "artikel":
+                            require BOUNDARIES . "article/article.php";
+                            break;
+
+                        /* INCLUDES VON KUNDENBEZIEHUNGSMANAGEMENT */
 
                         /* INCLUDES VON KUNDENBEZIEHUNGSMANAGEMENT */
                         case "kundeanlegen":
@@ -123,37 +121,38 @@ foreach (glob(INCS . "*.inc.php") as $filename) {
                             include('Berichtswesen/Personalreporting.php');
                             break;
 
-                                /* INCLUDES VON BERICHTSWESEN */
-                                case 'personalreporting':
-                                    include('Berichtswesen/Personalreporting.php');
-                                    break;
+                        /* INCLUDES VON BERICHTSWESEN */
+                        case 'personalreporting':
+                            include('Berichtswesen/Personalreporting.php');
+                            break;
 
-                                /* DEFAULT */
-                                default:
-                                    include(BOUNDARIES . "dashboard/dashboard.php");
-                                    break;
-                            }
-                        }
+                        /* DEFAULT */
+                        default:
+                            include(BOUNDARIES . "dashboard/dashboard.php");
+                            break;
                     }
-                    else {
-                        if(Session::get("username")) {
-                            require BOUNDARIES . "dashboard/dashboard.php";
-                        }
-                        else {
-                            require BOUNDARIES . "login/login.php";
-                        }
-                    }
-                    ?>
-                </div> <!-- end of content -->
-            </div> <!-- end of wrapper -->
+                }
+            } else {
+                if (Session::get("username")) {
+                    require BOUNDARIES . "dashboard/dashboard.php";
+                } else {
+                    require BOUNDARIES . "login/login.php";
+                }
+            }
+            ?>
+        </div>
+        <!-- end of content -->
+    </div>
+    <!-- end of wrapper -->
 
-            <!-- FOOTER, AUßERHALB VOM DIV "WRAPPER" -> FOOTER BLEIBT GANZ UNTEN  -->
-            <div id="footer">
-                <div id="footer-content">
-                    <a href="fpdf17/FAQ.htm">FAQ | Impressum</a>
-                </div>
-            </div>
+    <!-- FOOTER, AUßERHALB VOM DIV "WRAPPER" -> FOOTER BLEIBT GANZ UNTEN  -->
+    <div id="footer">
+        <div id="footer-content">
+            <a href="fpdf17/FAQ.htm">FAQ | Impressum</a>
+        </div>
+    </div>
 
-        </div> <!-- end of site -->
-    </body>
+</div>
+<!-- end of site -->
+</body>
 </html>
